80737b7826e1caf24cc26da2e09b909eef437bfe
Alexander Efremov <aedampir@gmail.com>
2016-02-19 01:24:26 +0700

    second commit

--- fem/utils.h
+++ fem/utils.h
@@ -1,11 +1,8 @@
 #ifndef FEM_CIRCLE_UTILS_H
 #define FEM_CIRCLE_UTILS_H
 
-#include <stdio.h>
-#include <float.h>
-
-inline void print_matrix_to_file(int n, int m, double *data, const char *filename) {
-    FILE *f = fopen(filename, "w");
+inline void print_matrix_to_file(int n, int m, double* data, const char* filename) {
+    FILE* f = fopen(filename, "w");
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             int k = i * n + j;
@@ -16,35 +13,42 @@ inline void print_matrix_to_file(int n, int m, double *data, const char *filenam
     fclose(f);
 }
 
-inline void print_int(const char *str, int i) {
+inline void print_int(const char* str, int i)
+{
     printf("%s %d\n", str, i);
 }
 
-inline void print_int_double(const char *str, int i, double d) {
+inline void print_int_double(const char* str, int i, double d)
+{
     printf("%s %d %f\n", str, i, d);
 }
 
-inline void print_double(const char *str, double d) {
+inline void print_double(const char* str, double d)
+{
     printf("%s %f\n", str, d);
 }
 
-inline void print_double(const char *str, double d1, double d2) {
+inline void print_double(const char* str, double d1, double d2)
+{
     printf("%s %f %f\n", str, d1, d2);
 }
 
-inline void print_float(const char *str, float d) {
+inline void print_float(const char* str, float d)
+{
     printf("%s %f\n", str, d);
 }
 
-inline void print_double_exp(const char *str, double d) {
+inline void print_double_exp(const char* str, double d)
+{
     printf("%s %e\n", str, d);
 }
 
-inline void print_float_exp(const char *str, float d) {
+inline void print_float_exp(const char* str, float d)
+{
     printf("%s %e\n", str, d);
 }
 
-inline void print_matrix(double *a, int n, int m, int precision = 8) {
+inline void print_matrix(double* a, int n, int m, int precision = 8) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             int k = i * n + j;
@@ -79,7 +83,7 @@ inline void print_matrix(double *a, int n, int m, int precision = 8) {
     }
 }
 
-inline void print_matrix(double *a, int n, int m, const char *text, int precision = 8) {
+inline void print_matrix(double* a, int n, int m, const char* text, int precision = 8) {
     printf("\n%s\n", text);
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
@@ -115,9 +119,11 @@ inline void print_matrix(double *a, int n, int m, const char *text, int precisio
     }
 }
 
-inline static void print_vector(double *a, int n, int precision = 8) {
-    for (int k = 0; k < n; ++k) {
-        switch (precision) {
+inline static void print_vector(double* a, int n, int precision = 8) {
+    for (int k = 0; k < n; ++k)
+    {
+        switch (precision)
+        {
             case 1:
                 printf("%.1f ", a[k]);
                 break;
@@ -144,6 +150,33 @@ inline static void print_vector(double *a, int n, int precision = 8) {
                 break;
         }
     }
+}
+
+double get_l1_norm(double hx, double hy, int x_len, int y_len, double* data)
+{
+    double r = 0.;
+    for (int i = 0; i < x_len; ++i)
+    {
+        for (int j = 0; j < y_len; ++j)
+        {
+            r += data[x_len * i + j];
+        }
+    }
+    return r * hx * hy;
+}
+
+double get_l_inf_norm(int x_len, int y_len, double* data)
+{
+    double max = FLT_MIN;
+    for (int i = 0; i < x_len; ++i)
+    {
+        for (int j = 0; j < y_len; ++j)
+        {
+            if (data[x_len * i + j] > max)
+                max = data[x_len * i + j];
+        }
+    }
+    return max;
 }
 
 #endif //FEM_CIRCLE_UTILS_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <string.h>
-#include <algorithm>
 #include <float.h>
+#include <algorithm>
 
 #ifdef WIN32
 __int64 timerStart = 0;
@@ -12,6 +12,7 @@ struct timeval timerStart;
 #endif
 
 #include "timer.h"
+#include "utils.h"
 #include "tecplot.h"
 
 // area is [AxB]x[CxD]
@@ -33,10 +34,13 @@ static double R_SQ; // radius of circle in second power
 static double INN_DENSITY; // density inside circle
 static double OUT_DENSITY; // density out of circle boundary
 
-inline static double analytical_solution_circle(double x, double y) {
-    double x0 = A + OX_LEN * HX / 2.;
-    double y0 = C + OY_LEN * HY / 2.;
-    double value = (x - x0) * (x - x0) + (y - y0) * (y - y0);
+#define EPS 10e-7
+
+inline static double analytical_solution_circle(double x, double y)
+{
+    double x0 = A + OX_LEN*HX/2.;
+    double y0 = C + OY_LEN*HY/2.;
+    double value = (x-x0)*(x-x0) + (y-y0)*(y-y0);
     if (value <= R_SQ) return INN_DENSITY;
     return OUT_DENSITY;
 }
@@ -45,15 +49,16 @@ inline static double func_u(double time_value, double x, double y) { return 0; }
 
 inline static double func_v(double time_value, double x, double y) { return 0; }
 
-static double get_phi(int ii, int jj, double *prev_density, double time_value) {
-    double x1 = A + ii * HX - HX / 2.;
-    double y1 = C + jj * HY - HY / 2.;
-    double x2 = A + ii * HX + HX / 2.;
-    double y2 = C + jj * HY - HY / 2.;
-    double x3 = A + ii * HX + HX / 2.;
-    double y3 = C + jj * HY + HY / 2.;
-    double x4 = A + ii * HX - HX / 2.;
-    double y4 = C + jj * HY + HY / 2.;
+static double get_phi(int ii, int jj, double* prev_density, double time_value)
+{
+    double x1 = A + ii*HX - HX/2.;
+    double y1 = C + jj*HY - HY/2.;
+    double x2 = A + ii*HX + HX/2.;
+    double y2 = C + jj*HY - HY/2.;
+    double x3 = A + ii*HX + HX/2.;
+    double y3 = C + jj*HY + HY/2.;
+    double x4 = A + ii*HX - HX/2.;
+    double y4 = C + jj*HY + HY/2.;
 
     double u = func_u(time_value, x1, y1);
     double v = func_v(time_value, x1, y1);
@@ -74,64 +79,69 @@ static double get_phi(int ii, int jj, double *prev_density, double time_value) {
 
     int nx = 10;
     int ny = 10;
-    double x_step = 1. / nx;
-    double y_step = 1. / ny;
+    double x_step = 1./nx;
+    double y_step = 1./ny;
 
     // get right part for jakoby
     double phi = 0.;
     double mes = x_step * y_step;
-    for (int i = 0; i < nx; ++i) {
-        for (int j = 0; j < ny; ++j) {
-            double ideal_x = i * x_step + x_step / 2.;
-            double ideal_y = j * y_step + y_step / 2.;
+    for(int i = 0; i < nx; ++i)
+    {
+        for(int j = 0; j < ny; ++j)
+        {
+            double ideal_x = i*x_step + x_step/2.;
+            double ideal_y = j*y_step + y_step/2.;
 
-            double real_x = x1 + (x2 - x1) * ideal_x + (x4 - x1) * ideal_y
-                            + (x1 + x3 - x2 - x4) * ideal_x * ideal_y;
-            double real_y = y1 + (y2 - y1) * ideal_x + (y4 - y1) * ideal_y
-                            + (y1 + y3 - y2 - y4) * ideal_x * ideal_y;
+            double real_x = x1 + (x2 - x1)*ideal_x + (x4 - x1)*ideal_y
+                            + (x1 + x3 - x2 - x4)*ideal_x*ideal_y;
+            double real_y = y1 + (y2 - y1)*ideal_x + (y4 - y1)*ideal_y
+                            + (y1 + y3 - y2 - y4)*ideal_x*ideal_y;
 
             // find out in which square real point was placed
-            int sq_i = (int) ((real_x - A) / HX);
-            int sq_j = (int) ((real_y - C) / HY);
+            int sq_i = (int)((real_x - A) / HX);
+            int sq_j = (int)((real_y - C) / HY);
             double x = A + sq_i * HX;
             double y = C + sq_j * HY;
 
             // formula 4
-            double dens = prev_density[sq_i * OY_LEN_1 + sq_j] * (1 - (real_x - x) / HX) * (1 - (real_y - y) / HY)
-                          + prev_density[(sq_i + 1) * OY_LEN_1 + sq_j] * ((real_x - x) / HX) * (1 - (real_y - y) / HY)
-                          + prev_density[(sq_i + 1) * OY_LEN_1 + sq_j + 1] * ((real_x - x) / HX) * ((real_y - y) / HY)
-                          + prev_density[sq_i * OY_LEN_1 + sq_j + 1] * (1 - (real_x - x) / HX) * ((real_y - y) / HY);
+            double dens = prev_density[sq_i * OY_LEN_1 + sq_j]*(1 - (real_x - x)/HX)*(1 - (real_y - y)/HY)
+                          + prev_density[(sq_i + 1) * OY_LEN_1 + sq_j]*((real_x - x)/HX)*(1 - (real_y - y)/HY)
+                          + prev_density[(sq_i + 1) * OY_LEN_1 + sq_j + 1]*((real_x - x)/HX)*((real_y - y)/HY)
+                          + prev_density[sq_i * OY_LEN_1 + sq_j + 1]*(1 - (real_x - x)/HX)*((real_y - y)/HY);
 
             double a11 = (x2 - x1) + (x1 + x3 - x2 - x4) * ideal_y;
             double a12 = (x4 - x1) + (x1 + x3 - x2 - x4) * ideal_x;
             double a21 = (y2 - y1) + (y1 + y3 - y2 - y4) * ideal_y;
             double a22 = (y4 - y1) + (y1 + y3 - y2 - y4) * ideal_x;
-            double jakob = a11 * a22 - a21 * a12;
+            double jakob = a11*a22 - a21*a12;
 
             phi += mes * dens * jakob;
         }
     }
 
-    phi *= 16. / 9.;
+    phi *= 16./9.;
     return phi;
 }
 
-static double *solve(double &tme) {
+static double* solve(double &tme)
+{
     StartTimer();
 
     fflush(stdout);
 
-    double *phi = new double[XY_LEN];
-    double *prev_density = new double[XY_LEN];
-    double *density = new double[XY_LEN];
+    double* phi = new double[XY_LEN];
+    double* prev_density = new double[XY_LEN];
+    double* density = new double[XY_LEN];
+    double maxErr = FLT_MIN;
 
     for (int i = 0; i < OX_LEN_1; i++)
         for (int j = 0; j < OY_LEN_1; j++)
-            prev_density[i * OY_LEN_1 + j] = analytical_solution_circle(HX * i, HY * j);
+            prev_density[i * OY_LEN_1 + j] = analytical_solution_circle(HX*i, HY*j);
 
     memcpy(density, prev_density, XY_LEN * sizeof(double));
 
-    for (int tl = 1; tl <= TIME_STEP_CNT; tl++) {
+    for (int tl = 1; tl <= TIME_STEP_CNT; tl++)
+    {
         // with usage of prev_density we calculate phi function values
         for (int i = 1; i < OX_LEN; ++i)
             for (int j = 1; j < OY_LEN; ++j)
@@ -144,97 +154,139 @@ static double *solve(double &tme) {
         // because we will use it in Jakoby method
         for (int i = 1; i < OX_LEN; ++i)
             for (int j = 1; j < OY_LEN; ++j)
-                prev_density[i * OY_LEN_1 + j] = 0.5;
+                prev_density[i * OY_LEN_1 + j] = 0.;
 
         int ic = 0;
-        while (ic < JAK_ITER_CNT) {
-            for (int i = 1; i < OX_LEN; ++i) {
-                for (int j = 1; j < OY_LEN; ++j) {
-                    density[i * OY_LEN_1 + j] = -1. / 9. * (
-                            1.5 * (
+        maxErr = FLT_MAX;
+        while(maxErr > EPS && ic < JAK_ITER_CNT)
+        {
+            for (int i = 1; i < OX_LEN; ++i)
+            {
+                for (int j = 1; j < OY_LEN; ++j)
+                {
+                    density[i * OY_LEN_1 + j] = -1./9.*(
+                            1.5*(
                                     prev_density[OY_LEN_1 * i + j - 1] + // left
                                     prev_density[OY_LEN_1 * (i - 1) + j] + // upper
                                     prev_density[OY_LEN_1 * i + j + 1] + // right
                                     prev_density[OY_LEN_1 * (i + 1) + j] // bottom
-                            ) + 0.25 * (
+                            ) + 0.25*(
                                     prev_density[OY_LEN_1 * (i + 1) + j + 1] + // bottom right
                                     prev_density[OY_LEN_1 * (i + 1) + j - 1] + // bottom left
                                     prev_density[OY_LEN_1 * (i - 1) + j - 1] + // upper right
                                     prev_density[OY_LEN_1 * (i - 1) + j + 1] // upper left
-                            )) + phi[OY_LEN_1 * i + j] / (HX * HY);
+                            )) + phi[OY_LEN_1 * i + j]/(HX*HY);
+                }
+            }
+            ++ic;
+            // print_matrix(density, OX_LEN_1, OY_LEN_1, 4);
+            // printf("%s\n", "\n");
+            // print_matrix(prev_density, OX_LEN_1, OY_LEN_1, 4);
+            maxErr = FLT_MIN;
+            for (int i = 1; i < OX_LEN; ++i)
+            {
+                for (int j = 1; j < OY_LEN; ++j)
+                {
+                    double val = fabs(density[i * OY_LEN_1 + j] - prev_density[i * OY_LEN_1 + j]);
+                    if (val > maxErr) {
+                        maxErr = val;
+                        //printf("%le\n", val);
+                    }
                 }
             }
             memcpy(prev_density, density, XY_LEN * sizeof(double));
-            ++ic;
         }
+
+        // if (tl == 1) {
+        // 	printf("EPS: %le max jak = %d\n", EPS, JAK_ITER_CNT);
+        // 	printf("Iterations: %d max err.: %le\n", ic, maxErr);
+        // 	if (ic >= JAK_ITER_CNT)
+        // 	{
+        // 		printf("%s\n", "Break on max Jakoby iteration exceed");
+        // 	}
+        // 	else if (maxErr < EPS)
+        // 	{
+        // 		printf("%s\n", "Break on l_inf exceed");
+        // 	}
+        // }
     }
 
     delete[] prev_density;
     delete[] phi;
-    tme = GetTimer() / 1000;
+    tme = GetTimer()/1000;
     return density;
 }
 
-double *calc_error(double hx, double hy, double *solution) {
-    double *res = new double[XY_LEN];
+double* calc_error(double hx, double hy, double* solution)
+{
+    double* res = new double[XY_LEN];
     for (int i = 0; i < OX_LEN_1; i++)
         for (int j = 0; j < OY_LEN_1; j++)
-            res[i * OY_LEN_1 + j] = fabs(solution[i * OY_LEN_1 + j]
-                                         - analytical_solution_circle(A + hx * i, C + hy * j));
+            res[i*OY_LEN_1 + j] = fabs(solution[i * OY_LEN_1 + j]
+                                       - analytical_solution_circle(A + hx*i, C + hy*j));
     return res;
 }
 
-double get_l1_norm(double hx, double hy, int x_len, int y_len, double *data) {
-    double r = 0.;
-    for (int i = 0; i < x_len; ++i) {
-        for (int j = 0; j < y_len; ++j) {
-            r += data[x_len * i + j];
-        }
-    }
-    return r * hx * hy;
-}
-
-double get_l_inf_norm(int x_len, int y_len, double *data) {
-    double max = FLT_MIN;
-    for (int i = 0; i < x_len; ++i) {
-        for (int j = 0; j < y_len; ++j) {
-            if (data[x_len * i + j] > max)
-                max = data[x_len * i + j];
-        }
-    }
-    return max;
-}
-
-int main() {
-    // double d = 10.;
-    double d = 50.;
-    // double d = 100.;
-    A = 0.;
-    B = 1.;
-    C = 0.;
-    D = 1.;
-
-    TAU = 1. / d;
-    OX_LEN = (int) d;
-    OY_LEN = (int) d;
-    OX_LEN_1 = OX_LEN + 1;
-    OY_LEN_1 = OY_LEN + 1;
-    HX = (B - A) / OX_LEN;
-    HY = (D - C) / OY_LEN;
-
-    JAK_ITER_CNT = 3000;
-    TIME_STEP_CNT = (int) d;
-    XY_LEN = OX_LEN_1 * OY_LEN_1;
-    R_SQ = 0.25 * 0.25;
-    INN_DENSITY = 1.;
-    OUT_DENSITY = 0.;
-
+int main()
+{
     double tme = 0.;
-    double *density = solve(tme);
-    double *err = calc_error(HX, HY, density);
-    print_surface_as_v("rho", OX_LEN, OY_LEN, HX, HY, TIME_STEP_CNT, A, C, density);
-    print_surface_as_v("err", OX_LEN, OY_LEN, HX, HY, TIME_STEP_CNT, A, C, err);
-    delete[] density;
-    delete[] err;
+
+    double d = 0;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        switch(i)
+        {
+            case 0:
+                d = 50.;
+                break;
+            case 1:
+                d = 100.;
+                break;
+            case 2:
+                d = 200.;
+                break;
+            case 3:
+                d = 400.;
+                break;
+            case 4:
+                d = 800.;
+                break;
+            case 5:
+                d = 1600.;
+                break;
+        }
+
+        A = 0.;
+        B = 1.;
+        C = 0.;
+        D = 1.;
+        R_SQ = 0.25*0.25;
+        INN_DENSITY = 1.;
+        OUT_DENSITY = 0.;
+        JAK_ITER_CNT = 3000;
+
+        TAU = 1./d;
+        OX_LEN = (int)d;
+        OY_LEN = (int)d;
+        OX_LEN_1 = OX_LEN + 1;
+        OY_LEN_1 = OY_LEN + 1;
+        HX = (B - A)/OX_LEN;
+        HY = (D - C)/OY_LEN;
+        TIME_STEP_CNT = (int)d;
+        XY_LEN = OX_LEN_1 * OY_LEN_1;
+        printf("OX_LEN = %d OY_LEN = %d\n", OX_LEN, OY_LEN);
+        double* density = solve(tme);
+        double* err = calc_error(HX, HY, density);
+        print_surface_as_v("rho", OX_LEN, OY_LEN, HX, HY, TIME_STEP_CNT, A, C, density);
+        print_surface_as_v("err", OX_LEN, OY_LEN, HX, HY, TIME_STEP_CNT, A, C, err);
+        double l1 = get_l1_norm(HX, HY, OX_LEN_1, OY_LEN_1, err);
+        double l_inf = get_l_inf_norm(OX_LEN_1, OY_LEN_1, err);
+        printf("l1 %le \n", l1);
+        printf("l_inf %le\n", l_inf);
+        delete[] density;
+        delete[] err;
+    }
+
     return 0;
 }
