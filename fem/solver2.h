e080a7ef7d35161be298764c163d9702a2504cae
Alexander Efremov <aedampir@gmail.com>
2016-03-19 17:00:18 +0700

    Добавил наметки для вывода 1д графика

--- fem/solver2.h
+++ fem/solver2.h
@@ -11,8 +11,4 @@ double get_center_y_2();
 
 double *get_exact_solution_2(double hx, double hy, double t);
 
-double calc_sum_rho(double *solution);
-
-double calc_sum_abs_rho(double *solution);
-
 #endif //FEM_CIRCLE_SOLVER_2_H
