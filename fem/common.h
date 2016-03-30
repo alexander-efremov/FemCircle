6d1af700c6e773b10867c54678c5778bdf48cc83
Alexander Efremov <aedampir@gmail.com>
2016-03-30 18:47:04 +0700

    Solver3 added

--- fem/common.h
+++ fem/common.h
@@ -20,4 +20,8 @@ double *solve_2(double &tme);
 double *calc_error_2(double hx, double hy, double tt, double *solution);
 double *get_exact_solution_2(double hx, double hy, double t);
 
+double *solve_3(double &tme);
+double *calc_error_3(double hx, double hy, double tt, double *solution);
+double *get_exact_solution_3(double hx, double hy, double t);
+
 #endif //FEM_CIRCLE_COMMON_H
